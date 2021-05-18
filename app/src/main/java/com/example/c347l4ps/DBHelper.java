0098fb8462c00c4b3fd487ac2090deedfc91c2cc
Stun4Life <weeweiren@gmail.com>
2021-05-18 15:07:31 +0800

    Anisa Our Queen :D :D

--- app/src/main/java/com/example/c347l4ps/DBHelper.java
+++ app/src/main/java/com/example/c347l4ps/DBHelper.java
@@ -67,7 +67,7 @@ public class DBHelper extends SQLiteOpenHelper {
                 String songSingers = cursor.getString(2);
                 int songYear = cursor.getInt(3);
                 int songStars = cursor.getInt(4);
-                Song song = new Song(id, songTitle, songSingers, songYear, songStars);
+                Song song = new Song(songTitle, songSingers, songYear, songStars);
                 songs.add(song);
             } while (cursor.moveToNext());
         }
@@ -88,7 +88,7 @@ public class DBHelper extends SQLiteOpenHelper {
         db.close();
         return result;
     }
-    public int deleteNote(int id){
+    public int deleteSong(int id){
         SQLiteDatabase db = this.getWritableDatabase();
         String condition = COLUMN_ID + "= ?";
         String[] args = {String.valueOf(id)};
